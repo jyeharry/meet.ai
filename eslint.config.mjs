ca3d72494d06baa9cdf78e833f57c27a146e1dde
Jye Harry <jyeharry21@hotmail.com>
2025-11-06 22:27:34 +0000

    02 database

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: { quotes: ["warn", "double"], }
+  }
 ];
 
 export default eslintConfig;
