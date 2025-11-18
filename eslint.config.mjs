041842e017f03fff6147501ed7a47765d0f0d3b8
Jye Harry <jyeharry21@hotmail.com>
2025-11-18 23:03:26 +0000

    03 auth setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: { quotes: ["warn", "double"], }
+    rules: { quotes: ["warn", "double"], "jsx-quotes": ["warn", "prefer-double"], semi: ["warn", "never"] }
   }
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,86 @@
-import { Button } from "@/components/ui/button";
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { authClient } from "@/lib/auth-client"
+import { useState } from "react"
 
 export default function Home() {
+  const [name, setName] = useState("") // Adding name state
+  const [email, setEmail] = useState("") // Adding email state
+  const [password, setPassword] = useState("") // Adding password state
+
+  const {
+    data: session,
+  } = authClient.useSession()
+
+  if (session) {
+    return (
+      <div className="flex flex-col p-4 gap-y-4">
+        <p>Logged in as {session.user.name}</p>
+        <Button onClick={() => authClient.signOut()}>Sign out</Button>
+      </div >
+    )
+  }
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email,
+      password,
+      name,
+    },
+      {
+        onError: () => window.alert("Something went wrong"),
+        onSuccess: () => window.alert("Success")
+      })
+  }
+
+  const onSignIn = () => {
+    authClient.signIn.email({
+      email,
+      password,
+    },
+      {
+        onError: () => window.alert("Something went wrong"),
+        onSuccess: () => window.alert("Success")
+      })
+  }
+
   return (
-    <Button>Click me</Button>
+    <div className="flex flex-col p-4 gap-y-4">
+      <div className="flex flex-col p-4 gap-y-4">
+        <Input
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={() => onSubmit()}>Create user</Button>
+      </div>
+      <div className="flex flex-col p-4 gap-y-4">
+        <Input
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={() => onSignIn()}>Login</Button>
+      </div>
+    </div>
   )
 }
