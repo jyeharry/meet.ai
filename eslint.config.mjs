4331c83a658e821a614cd91ee748e02911a29590
Jye Harry <jyeharry21@hotmail.com>
2025-11-24 21:53:55 +0000

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,10 +10,13 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
   {
-    rules: { quotes: ["warn", "double"], "jsx-quotes": ["warn", "prefer-double"], semi: ["warn", "never"] }
-  }
+    rules: {
+      quotes: ["warn", "double"],
+      "jsx-quotes": ["warn", "prefer-double"],
+    },
+  },
 ]
 
 export default eslintConfig
